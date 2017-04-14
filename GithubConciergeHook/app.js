6c86102b41e1cee76f14b67d85632c3cca06e590
Matthew Knox <mrk45@uclive.ac.nz>
2017-04-14 16:18:46 +1200

    Updates

--- GithubConciergeHook/app.js
+++ GithubConciergeHook/app.js
@@ -14,23 +14,38 @@ const getPackageJson = (name, branch, callback) => {
 };
 
 const sendStatus = (status, event) => {
-    console.log(status);
-    console.log(event);
+    const data = {
+        status: status,
+        context: 'package.json Version',
+        description: status === 'success' ? 'The version number has been updated.' : 'Please update the package.json version number.'
+    };
+    request.post({
+        url: event.payload.pull_request.statuses_url,
+        headers: {
+            Authorization: `token ${config.token}`,
+            'Content-Type': 'application/json',
+            'User-Agent': 'GithubConciergeHook',
+            body: data
+        }
+    }, (_, __, body) => {
+        console.log(body);
+    });
 };
 
 const verifyStatus = (event, master, pr) => {
-    sendStatus(semver.lt(master.version, pr.version), event);
+    sendStatus(semver.lt(master.version, pr.version) ? 'success' : 'failure', event);
 };
 
 server.webhookHandler.on('pull_request', event => {
-    console.log(event);
+    sendStatus('pending', event);
+
     const name = event.payload.repository.full_name;
     const master = event.payload.repository.default_branch;
     const remoteName = event.payload.pull_request.head.repo.full_name;
     const current = event.payload.pull_request.head.ref;
 
     if (remoteName === name && master === current) {
-        return sendStatus(true, event);
+        return sendStatus('success', event);
     }
 
     getPackageJson(name, master, masterPackageJson => {
