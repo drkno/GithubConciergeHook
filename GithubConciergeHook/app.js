298120c2e0bf4773a94fedac1394afad5f016c6b
Matthew Knox <mrk45@uclive.ac.nz>
2017-04-14 14:15:04 +1200

    Initial

--- GithubConciergeHook/app.js
+++ GithubConciergeHook/app.js
@@ -1,4 +1,12 @@
 'use strict';
 
-const config = require('config.json');
+const config = require('./config.json');
 const webhooks = require('github-webhook');
+
+const server = webhooks(config);
+
+server.webhookHandler.on('pull_request', event => {
+    console.log(event);
+});
+
+server.listen(config.port);
